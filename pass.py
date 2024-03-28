0d0f1cb2288a5da1e00bc4d3c92a7fe233e2c034
BluePlTheDeer_ <164904476+BluePlTheDeer251@users.noreply.github.com>
2024-03-28 11:45:23 -0600

    Create pass.py

---
+++ pass.py
@@ -0,0 +1,17 @@
+print("Welcome to Pygle, Enter your username and password.")
+entered_User = input("Username: ")
+enteredPass = input("Password: ")
+inSystem_User = "Test_1234"
+inSystem_pass = 251045
+username = entered_User == inSystem_User
+password = int(enteredPass) == inSystem_pass
+print(username)
+if username is True:
+    print("Valid Username, but the password?")
+else:
+    print("No user found with that username, I guess you wrote it wrong or just tried to hack someone.")
+print(password)
+if password is True:
+    print("Ok, go in, I just don't know how to make a JavaScript (or even HTML/CSS or PHP) UI.")
+else:
+    print("Are you trying to hack someone to get their personal info? You have been reported to the FBI.")
