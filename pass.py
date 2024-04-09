a5d26a5c55bddb446273d65f64505a4d65193945
BluePlTheDeer_ <164904476+BluePlTheDeer251@users.noreply.github.com>
2024-04-08 19:05:00 -0600

    Update pass.py

--- pass.py
+++ pass.py
@@ -1,4 +1,4 @@
-print("Welcome to Pygle, Enter your username and password.")
+print("Welcome to Pyrofox, Enter your username and password.")
 entered_User = input("Username: ")
 enteredPass = input("Password: ")
 inSystem_User = "Test_1234"
@@ -9,9 +9,9 @@ print(username)
 if username is True:
     print("Valid Username, but the password?")
 else:
-    print("No user found with that username, I guess you wrote it wrong or just tried to hack someone.")
+    print("No user found with that username, I guess you wrote it wrong or just tried to hack someone in here.")
 print(password)
 if password is True:
     print("Ok, go in, I just don't know how to make a JavaScript (or even HTML/CSS or PHP) UI.")
 else:
-    print("Are you trying to hack someone to get their personal info? You have been reported to the FBI.")
+    print("Are you trying to hack someone on this device to get their personal info? You have been reported to the FBI.")
